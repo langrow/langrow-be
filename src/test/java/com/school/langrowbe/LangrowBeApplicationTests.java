b2f7f3ae968f122269ab5f6d5d3f2f1712d6aaaa
김나경 <1030n@naver.com>
2025-12-01 19:28:57 +0900

    :tada: Start: langrow-backend project

---
+++ src/test/java/com/school/langrowbe/LangrowBeApplicationTests.java
@@ -0,0 +1,11 @@
+package com.school.langrowbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangrowBeApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
